96b9454d92b08c242d1163c0dcd136317acc8bb0
Tristan B. Kildaire <tristankildaire@gmail.com>
2014-11-23 13:56:53 +0200

    Lessened code.

--- libblockchain.py
+++ libblockchain.py
@@ -4,139 +4,79 @@
 import urllib
 
 def get_balance(address):
-    reader = urllib.request.urlopen("http://blockchain.info/q/addressbalance/" + address)
-    balance = reader.read()
-    reader.close()
-    return balance
+    return urllib.request.urlopen("http://blockchain.info/q/addressbalance/" + address).read()
 
 def get_amount_received(address):
-    reader = urllib.request.urlopen("http://blockchain.info/q/getreceivedbyaddress/" + address)
-    amnt_received = reader.read()
-    reader.close()
-    return balance
+    return urllib.request.urlopen("http://blockchain.info/q/getreceivedbyaddress/" + address).read()
 
 def get_amount_sent(address):
-    reader = urllib.request.urlopen("http://blockchain.info/q/getsentbyaddress/" + address)
-    amnt_sent = reader.read()
-    reader.close()
-    return balance
+    return urllib.request.urlopen("http://blockchain.info/q/getsentbyaddress/" + address).read()
 
 'def check_address(address):'
-    'reader = urllib.request.urlopen("http://dogechain.info/chain/Dogecoin/q/checkaddress/" + address)'
-    'output = reader.read()'
-    'reader.close()'
+    'return urllib.request.urlopen("http://dogechain.info/chain/Dogecoin/q/checkaddress/" + address)'
+    ''
+    ''
     'if output == "X5" or output == "SZ" or output == "CK": return False'
     'else: return True'
 
 def decode_address(address):
-    reader = urllib.request.urlopen("http://dogechain.info/chain/Dogecoin/q/decode_address/" + address)
-    output = reader.read()
-    reader.close()
-    return output
-    
+    return urllib.request.urlopen("http://dogechain.info/chain/Dogecoin/q/decode_address/" + address).read()
+
 def get_address_firstseen(address):
-    reader = urllib.request.urlopen("http://blockchain.info/q/addressfirstseen/" + address)
-    output = reader.read()
-    reader.close()
-    return output
+    return return urllib.request.urlopen("http://blockchain.info/q/addressfirstseen/" + address).read()
 
 def address_to_hash(address):
-    reader = urllib.request.urlopen("http://blockchain.info/q/addresstohash/" + address)
-    output = reader.read()
-    reader.close()
-    return output
+    return urllib.request.urlopen("http://blockchain.info/q/addresstohash/" + address).read()
 
 def hash_to_address(address):
-    reader = urllib.request.urlopen("http://blockchain.info/q/hashtoaddress/" + address)
-    output = reader.read()
-    reader.close()
-    return output
-    
+    return urllib.request.urlopen("http://blockchain.info/q/hashtoaddress/" + address).read()
+
 def get_difficulty():
-    reader = urllib.request.urlopen("http://dogechain.info/chain/Dogecoin/q/getdifficulty")
-    output = reader.read()
-    reader.close()
-    return output
+    return urllib.request.urlopen("http://dogechain.info/chain/Dogecoin/q/getdifficulty").read()
 
 def get_eta_nextblock():
-    reader = urllib.request.urlopen("http://blockchain.info/q/eta")
-    output = reader.read()
-    reader.close()
-    return output
-    
+    return urllib.request.urlopen("http://blockchain.info/q/eta").read()
+
 def get_avgtx_number():
-    reader = urllib.request.urlopen("http://blockchain.info/q/avgtxnumber")
-    output = reader.read()
-    reader.close()
-    return output
+    return urllib.request.urlopen("http://blockchain.info/q/avgtxnumber").read()
 
 def get_block_count():
-    reader = urllib.request.urlopen("http://blockchain.info/q/getblockcount")
-    output = reader.read()
-    reader.close()
-    return output
-    
+    return urllib.request.urlopen("http://blockchain.info/q/getblockcount").read()
+
 def get_total_bc():
-    reader = urllib.request.urlopen("http://blockchain.info/q/totalbc")
-    output = reader.read()
-    reader.close()
-    return output
+    return urllib.request.urlopen("http://blockchain.info/q/totalbc").read()
 
 def get_hashrate():
     'Get the hashrate in gigahashes.'
-    reader = urllib.request.urlopen("http://blockchain.info/q/hashrate")
-    output = reader.read()
-    reader.close()
-    return output
-    
+    return urllib.request.urlopen("http://blockchain.info/q/hashrate").read()
+
 def get_latest_hash():
     'Get the hashrate in gigahashes.'
-    reader = urllib.request.urlopen("http://blockchain.info/q/latesthash")
-    output = reader.read()
-    reader.close()
-    return output
+    return urllib.request.urlopen("http://blockchain.info/q/latesthash").read()
 
 def get_market_cap():
     'Get the hashrate in gigahashes.'
-    reader = urllib.request.urlopen("http://blockchain.info/q/marketcap")
-    output = reader.read()
-    reader.close()
-    return output
+    return urllib.request.urlopen("http://blockchain.info/q/marketcap").read()
 
 def get_transactions_count_24hr():
     'Get the hashrate in gigahashes.'
-    reader = urllib.request.urlopen("http://blockchain.info/q/24hrtransactioncount")
-    output = reader.read()
-    reader.close()
-    return output
+    return urllib.request.urlopen("http://blockchain.info/q/24hrtransactioncount").read()
 
 def get_btc_sent_count_24hr_satoshi():
     'Get the hashrate in gigahashes.'
-    reader = urllib.request.urlopen("http://blockchain.info/q/24hrbtcsent")
-    output = reader.read()
-    reader.close()
-    return output
+    return urllib.request.urlopen("http://blockchain.info/q/24hrbtcsent").read()
 
 def get_unconfirmed_count():
     'Get the hashrate in gigahashes.'
-    reader = urllib.request.urlopen("http://blockchain.info/q/unconfirmedcount")
-    output = reader.read()
-    reader.close()
-    return output
+     return urllib.request.urlopen("http://blockchain.info/q/unconfirmedcount").read()
 
 def generate_key():
     'Generate a new bitcoin address and a private key. Returns public key and then private key.'
-    reader = urllib.request.urlopen("http://blockchain.info/q/newkey")
-    output = reader.read()
-    reader.close()
-    return output
+    return urllib.request.urlopen("http://blockchain.info/q/newkey").read()
 
 def get_unconfirmed_count():
     'Get the hashrate in gigahashes.'
-    reader = urllib.request.urlopen("http://blockchain.info/q/unconfirmedcount")
-    output = reader.read()
-    reader.close()
-    return output
+    return urllib.request.urlopen("http://blockchain.info/q/unconfirmedcount").read()
 
 def get_version():
     'Get the version of the library.'
